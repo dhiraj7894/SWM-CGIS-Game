146b74078fccd7d5da0cc4cfed29c6572eba1c91
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2020-02-13 23:24:23 +0530

    Update 13-Feb-2020 11:24

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    float score, escore, wscore, dscore, snscore;
+    public GameObject snw, ew, dw, ww;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score";
+    }
+
+    // Update is called once per frame
+    void Update() {
+        scoreText.text = escore.ToString();
+        if (E_Waste._locked && WetWaste._locked && dryWaste._locked && SanNHazWaste._locked)
+        {
+            score = escore + wscore + dscore + snscore;
+            scoreText.text = "Score is : " + score.ToString();
+        }
+
+        
+    }
+    public void E_WasteScore(float score)
+    {
+        escore = score;
+        Debug.Log("E-Waste : " + escore);
+    }
+    public void WWasteScore(float score)
+    {
+        wscore = score;
+        Debug.Log("W-Waste : " + wscore);
+    }
+    public void DWasteScore(float score)
+    {
+        dscore = score;
+        Debug.Log("D-Waste : " + dscore);
+    }
+    public void SNWasteScore(float score)
+    {
+        
+        snscore = score;
+        Debug.Log("SN-Waste : " + snscore +" "+ dscore + " " + escore + " " + wscore);
+    }
+}
---
+++ Assets/Script/Garbadge/SanNHazWaste.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SanNHazWaste : MonoBehaviour
+{
+    private GameManager gameManager;
+    private float scoreData = 0f;
+    private float chanceData = 3f;
+    public static float score;
+
+
+
+    public Transform _wWastePlace, _eWastePlace, _sHWastePlace, _dWastePlace;
+    private Vector2 _intialPlace;
+    private float _deltaX, _deltaY;
+    public float b;
+    public static bool _locked, _isSelected = false;
+
+    public void Awake()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    void Start()
+    {
+        b = 1f;
+        Vector3 mousePosition = Input.mousePosition;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        _intialPlace = transform.position;
+    }
+
+    void Update()
+    {
+        if (_isSelected == true)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        }
+        else if (_isSelected == false)
+        {
+            if (Mathf.Abs(transform.position.x - _sHWastePlace.position.x) <= b && Mathf.Abs(transform.position.y - _sHWastePlace.position.y) <= b)
+            {
+                if (_sHWastePlace.name == "SNWaste")
+                {
+                    transform.position = new Vector2(_sHWastePlace.position.x, _sHWastePlace.position.y);
+                    _locked = true;
+
+
+                    score = scoreData;
+                    if (chanceData == 3)
+                    {
+                        scoreData = 4;
+                        Destroy(gameObject);
+                        gameManager.SNWasteScore(scoreData);
+                        _locked = true;
+                        //Debug.LogWarning(scoreData);
+                    }
+                    if (chanceData == 2)
+                    {
+                        scoreData = 3;
+                        Destroy(gameObject);
+                        gameManager.SNWasteScore(scoreData);
+                        _locked = true;
+                        //Debug.LogWarning(scoreData);
+                    }
+                    if (chanceData == 1)
+                    {
+                        scoreData = 2;
+                        Destroy(gameObject);
+                        gameManager.SNWasteScore(scoreData);
+                        _locked = true;
+                        //Debug.LogWarning(scoreData);
+                    }
+                    if (chanceData == 0)
+                    {
+                        scoreData = 1;
+                        Destroy(gameObject);
+                        gameManager.SNWasteScore(scoreData);
+                        _locked = true;
+                        //Debug.LogWarning(scoreData);
+                    }
+                    if (chanceData < 0)
+                    {
+                        _locked = false;
+                    }
+
+                }
+            }
+            if (Mathf.Abs(transform.position.x - _wWastePlace.position.x) <= b && Mathf.Abs(transform.position.y - _wWastePlace.position.y) <= b)
+            {
+                //Debug.Log("WW");
+                transform.position = new Vector2(_intialPlace.x, _intialPlace.y);
+                if (_wWastePlace.name == "WWaste")
+                {
+                    chanceData--;
+                    //Debug.Log("Error" + chanceData);
+                }
+            }
+            if (Mathf.Abs(transform.position.x - _eWastePlace.position.x) <= b && Mathf.Abs(transform.position.y - _eWastePlace.position.y) <= b)
+            {
+                //Debug.Log("EW");
+                transform.position = new Vector2(_intialPlace.x, _intialPlace.y);
+                if (_eWastePlace.name == "EWaste")
+                {
+                    chanceData--;
+                    //Debug.Log("Error" + chanceData);
+                }
+            }
+            if (Mathf.Abs(transform.position.x - _dWastePlace.position.x) <= b && Mathf.Abs(transform.position.y - _dWastePlace.position.y) <= b)
+            {
+                //Debug.Log("DW");
+                transform.position = new Vector2(_intialPlace.x, _intialPlace.y);
+                if (_dWastePlace.name == "DWaste")
+                {
+                    chanceData--;
+                    //Debug.Log("Error" + chanceData);
+                }
+            }
+
+            else
+            {
+                transform.position = new Vector2(_intialPlace.x, _intialPlace.y);
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+        Vector3 mousePosition = Input.mousePosition;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        _deltaX = mousePos.x - transform.localPosition.x;
+        _deltaY = mousePos.y - transform.localPosition.y;
+        _isSelected = true;
+    }
+    private void OnMouseUp()
+    {
+        _isSelected = false;
+    }
+}
