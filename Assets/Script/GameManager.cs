0e477eb80a845c0b64bd27661985f7a03cb51404
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2020-03-15 06:24:31 +0530

    Update
    Added comments of scripts
    Adding XML Serilizable

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+
+public class GameManager : MonoBehaviour
+{
+    public  string category;
+    public  string name_object;
+    public  float score_object;
+    dataEntry dE = new dataEntry();
+    
+    
+    
+    
+    void FixedUpdate()
+    {
+        
+        dE.category = category;
+        dE.object_name = name_object;
+        dE.object_score = score_object;
+    }
+    public ItemDatabase itemDB;
+    // save function
+    // create and open xml file 
+    // save category, name, score data into xml file
+    // close xml file
+    public void saveItem()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(ItemDatabase));
+        FileStream stream = new FileStream(Application.dataPath + "/RowData/XmlData/itemData.xml", FileMode.Create);
+        serializer.Serialize(stream, itemDB);
+        stream.Close();
+    }
+
+    //load dunction
+    public void loadItem()
+    {
+
+    }
+
+
+        //Get the datas from every script like e-watse, wet-waste and others....
+        //set score, name, category data to dataEntry class
+    public void E_WasteScore(float score, string name, string cat)
+    {
+        saveItem();
+        Debug.Log(dE.category);
+        category = cat;
+        name_object = name;
+        score_object = score;
+        Debug.Log(cat + " __ " + name + " __ " + score);
+    }
+    public void WWasteScore(float score, string name, string cat)
+    {
+        saveItem();
+        Debug.Log(dE.category);
+        category = cat;
+        name_object = name;
+        score_object = score;
+        Debug.Log(cat + " __ " + name + " __ " + score);
+    }
+    public void DWasteScore(float score, string name, string cat)
+    {
+        saveItem();
+        Debug.Log(dE.category);
+        category = cat;
+        name_object = name;
+        score_object = score;
+        Debug.Log(cat + " __ " + name + " __ " + score);
+    }
+    public void SNWasteScore(float score, string name, string cat)
+    {
+        saveItem();
+        Debug.Log(dE.category);
+        category = cat;
+        name_object = name;
+        score_object = score;
+        Debug.Log(cat + " __ " + name + " __ " + score);
+    }
+
+    
+
+}
+[System.Serializable]
+public class dataEntry {
+
+    public string category;
+    public string object_name;
+    public float object_score;
+
+}
+
+
+[System.Serializable]
+public class ItemDatabase {
+    public List<dataEntry> list = new List<dataEntry>();
+}
---
+++ Assets/Script/Garbadge/dryWaste.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class dryWaste : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private Vector2 _intialPlace;
+    private CanvasGroup canvasGroup;
+    private GameManager gameManager;
+    private float scoreData = 0f;
+    private float chanceData = 3f;
+    public static float score;
+    public static bool locked;
+    public string name, cat;
+
+    public void Awake()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        _intialPlace = rectTransform.anchoredPosition;
+    }
+
+    //when we start dragging a garbadge just set alpha value 0.6 so it will look like little bit transperent.
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnBefinDrag");
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    //when we drag a object just keep object on the position of mouse pointer.
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+
+
+    //on the stop of drag just check 
+    //1. set object alpha 1 so it will not transperent.
+    //2. check where the my garbadge is on which dustbin.
+    //3. if dustbin is any of other than Dry-Waste then place the garbadge position initial.
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+        // if garbadge is on Dry-Waste dustbin
+        // set the score adding the chance of player
+        // and send the Score, Name of garbadge and Category of garbadge at game Manager
+
+        if (eventData.pointerEnter.name == "DWST")
+        {
+            scoreData = chanceData + 1;
+            FindObjectOfType<GameManager>().saveItem();
+            gameManager.E_WasteScore(scoreData, name, cat);
+            locked = true;
+            Destroy(gameObject, 0.1f);
+        }
+        // if garbadge is placed on other dustbin just decrease the chance and place the garbadge at initial position.
+        else if (eventData.pointerEnter.name == "EWST" || eventData.pointerEnter.name == "WWST" || eventData.pointerEnter.name == "SnHWST")
+        {
+            chanceData--;
+            rectTransform.anchoredPosition = new Vector2(_intialPlace.x, _intialPlace.y);
+            Debug.Log("Error");
+        }
+
+
+        // else place the position of garbadge on initial.
+        else
+        {
+            rectTransform.anchoredPosition = new Vector2(_intialPlace.x, _intialPlace.y);
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+        //Debug.Log("OnPointerDown");
+    }
+}
